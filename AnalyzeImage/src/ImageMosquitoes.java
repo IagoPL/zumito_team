9ec57fdcf9a5d6e21f6c4b6b8b8faa7057b3cc60
MarinaPifarre <92932322+MarinaPifarre@users.noreply.github.com>
2021-11-27 17:07:51 +0100

    Add files via upload
    hsl hecho

--- AnalyzeImage/src/ImageMosquitoes.java
+++ AnalyzeImage/src/ImageMosquitoes.java
@@ -8,7 +8,6 @@ import javax.imageio.ImageIO;
 
 public class ImageMosquitoes {
 	public static void main(String[] args) {
-		System.out.println(41/16);
 		File file = new File("C:\\Users\\Asus\\eclipse-workspace\\AnalyzeImage\\src\\imagen.png");
 		try {
 			Color color;
@@ -18,6 +17,11 @@ public class ImageMosquitoes {
 			int  green;
 			int  blue;
 			image = ImageIO.read(file);
+			int max;
+			int min;
+			int hue;
+			int luminosidad;
+			int saturacion;
 			int numPixelsHoritzontal = image.getWidth();
 			int numPixelsVertical = image.getHeight();
 			String colorEnHex = "";
@@ -28,13 +32,11 @@ public class ImageMosquitoes {
 					green = (c & 0x0000ff00) >> 8;
 					blue = c & 0x000000ff;
 					color = new Color(red, green, blue);
-					//colorEnHex = colorEnHex + pasarDeDecAHex(red);
-					//colorEnHex = colorEnHex + pasarDeDecAHex(green);
-					//colorEnHex = colorEnHex + pasarDeDecAHex(blue);
-					/*if (color.getRed()==0 && color.getBlue()==0 && color.getGreen()==0) {
-						System.out.println("X: " + coordenadaX);
-						System.out.println("Y: " + coordenadaY + "\n\n");
-					}*/
+					max = calcularMax(red, green, blue);
+					min = calcularMin(red, green, blue);
+					hue = obtenerHue(max, min, red, green, blue);
+					luminosidad = obtenerLuminosidad(max, min);
+					saturacion = obtenerSaturacion(max, min, luminosidad);
 				}
 			}
 		} catch (IOException e) {
@@ -42,77 +44,74 @@ public class ImageMosquitoes {
 		}
 	}
 	
-	/*public static String pasarDeDecAHex (int numeroDecimal) {
-		ArrayList<Integer> digitosHex = new ArrayList<>();
-		int numADividir = numeroDecimal;
-		int cociente = -1;
-		int residuo;
-		while (cociente != 0) {
-			cociente = numADividir/16;
-			residuo = numADividir - cociente * 16;
-			digitosHex.add(residuo);
-			numADividir = cociente;
-		}
-		digitosHex.add(numADividir);
-		String valorHexadecimal = "";
-		for (int i=digitosHex.size()-1; i>=0; i--) {
-			if (digitosHex.get(i)>=0 && digitosHex.get(i)<=9) {
-				valorHexadecimal = valorHexadecimal + digitosHex.get(i);
-			} else if (digitosHex.get(i)>=10 && digitosHex.get(i)<=15) {
-				valorHexadecimal = valorHexadecimal + digitoDecimalAHex(digitosHex.get(i));
+	public static int calcularMax (int red, int green, int blue) {
+		int max;
+		if (red>=green) {
+			if (red>=blue) {
+				max = red;
 			} else {
-				System.out.println("El digito no esta entre 0 y 15");
+				max = blue;
+			}
+		} else {
+			if (green>=blue) {
+				max = green;
+			} else {
+				max = blue;
 			}
 		}
-		return valorHexadecimal;
-	}*/
+		return max;
+	}
 	
-	/*public static String digitoDecimalAHex (int digito) {
-		String digitoARetornar = "";
-		switch (digito) {
-		case 10:
-			digitoARetornar = "A";
-			break;
-		case 11:
-			digitoARetornar = "B";
-			break;
-		case 12:
-			digitoARetornar = "C";
-			break;
-		case 13:
-			digitoARetornar = "D";
-			break;
-		case 14:
-			digitoARetornar = "E";
-			break;
-		case 15:
-			digitoARetornar = "F";
-			break;
-		default:
-			System.out.println("El digito no esta entre 10 y 15");
+	public static int calcularMin (int red, int green, int blue) {
+		int min;
+		if (red<=green) {
+			if (red<=blue) {
+				min = red;
+			} else {
+				min = blue;
+			}
+		} else {
+			if (green<=blue) {
+				min = green;
+			} else {
+				min = blue;
+			}
 		}
-		return digitoARetornar;
-	}*/
+		return min;
+	}
 	
-	public static void obtenerColor (int red, int green, int blue) {
-		//String color;
-		/*if (red==0 && green==0 && blue==0) {
-			color = "blanco";
-		} else if (red==0 && green==0) {
-			color = "azul";
-		} else if (green==0 && blue==0) {
-			color = "rojo";
-		} else if (red==0 && blue==0) {
-			color = "verde";
-		} else if (red==0) {
-			color = "cian";
-		} else if (green==0) {
-			color = "magenta";
-		} else if (blue==0) {
-			color = "amarillo";
-		} else if (red==green && green==blue && blue==red) {
-			color = "gris";
-		}*/
-		
+	public static int obtenerHue (int max, int min, int red, int green, int blue) {
+		int hue = -1;
+		if (max == min) { //esto es gris
+			hue = 0;
+		} else if (max == red) {
+			hue = (60*(green-blue)/(max-min)+360)%360;
+		} else if (max == green) {
+			hue = 60*(blue-red)/(max-min)+120;
+		} else if (max == blue) {
+			hue = 60*(red-green)/(max-min)+240;
+		} else {
+			System.out.println("Error al obtener el hue, no es ninguno de los cuatro casos");
+		}
+		return hue;
+	}
+	
+	public static int obtenerLuminosidad (int max, int min) {
+		int luminosidad = (max+min)/2;
+		return luminosidad;
+	}
+	
+	public static int obtenerSaturacion (int max, int min, int luminosidad) {
+		int saturacion = -1;
+		if (max == min) {
+			saturacion = 0;
+		} else if (luminosidad <= 1/2) {
+			saturacion = (max-min)/2*luminosidad;
+		} else if (luminosidad>1/2) {
+			saturacion = (max-min)/(2-2*luminosidad);
+		} else {
+			System.out.println("Error al obtener la luminosidad, no es ninguno de los tres casos");
+		}
+		return saturacion;
 	}
 }
